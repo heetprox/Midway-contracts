38d31db039efefaba15265329830b1bbbc759699
virtualheet <heetvavadiya456@gmail.com>
2025-08-24 13:35:46 +0530

    feat(router): add ExternalRouter contract with dependencies

---
+++ contracts/router/ExternalRouter.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ILayerZeroEndpoint} from "@layerzerolabs/solidity-examples/contracts/lzApp/interfaces/ILayerZeroEndpoint.sol";
+import {IExternalRouter} from "../interfaces/IExternalRouter.sol";
+
+
