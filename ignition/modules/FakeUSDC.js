7bd839067f866538534361fa5a308ed44030e4d6
virtualheet <heetvavadiya456@gmail.com>
2025-08-26 18:04:48 +0530

    refactor: update deployed addresses and journal entries for FakeUSDC and MidPayClient modules
    - Update the deployed address for FakeUSDCModule to the new contract address.
    - Remove outdated chain-919 and chain-999999999 deployment files and modules.
    - Adjust journal entries to reflect the latest transaction details and interactions for the updated modules.

--- ignition/modules/FakeUSDC.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-const FakeUSDCModule = buildModule("FakeUSDCModule", (m) => {
-  const fakeUSDC = m.contract("FakeUSDC", []);
-
-  return { fakeUSDC };
-});
-
-module.exports = FakeUSDCModule;
